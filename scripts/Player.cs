ff011e399ba8b9d7c34cbb6429179c01fcd5fd4c
ihorekghost <158592015+ihorekghost@users.noreply.github.com>
2024-02-09 06:42:54 +0200

    Added head swinging animation to player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,23 +3,29 @@ using System;
 
 public partial class Player : RigidBodyEx3D
 {
-    [ExportCategory("Motion")]
+    [ExportGroup("Animation")]
+    private float _moveAnimationSensitivity = 0.25f;
+
+    [ExportGroup("Motion")]
     [Export]
     public float Speed = 50.0f;
 
     [Export]
     public float JumpForce = 4.0f;
 
-    [ExportCategory("Controls")]
+    [ExportGroup("Controls")]
     [Export]
-    public float MouseSensivity = 0.01f;
+    public float MouseSensitivity = 0.01f;
 
-    [ExportCategory("References")]
+    [ExportGroup("References")]
     [Export]
     public Node3D Neck { get; private set; } = null;
 
     [Export]
     public Camera3D Head { get; private set; } = null;
+
+    [Export]
+    public AnimationTree AnimationTree { get; private set; } = null;
 
     public override void _Ready()
     {
@@ -30,9 +36,17 @@ public partial class Player : RigidBodyEx3D
     {
         base._IntegrateForces(state);
 
-        if (Input.IsActionJustPressed("jump") && IsOnFloor) ApplyCentralImpulse(Vector3.Up * JumpForce);
+        if (Input.IsActionJustPressed("jump") && IsOnFloor) state.ApplyCentralImpulse(Vector3.Up * JumpForce);
 
         state.ApplyCentralForce(Neck.Basis * new Vector3(Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"), 0.0f, Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward")).Normalized() * Speed);
+
+        //Animation
+
+        float blend = Mathf.Min(state.LinearVelocity.Length() * _moveAnimationSensitivity, 1.0f);
+
+        GD.Print(blend);
+
+        AnimationTree.Set("parameters/blend_position", blend);
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -40,8 +54,8 @@ public partial class Player : RigidBodyEx3D
         InputEventMouseMotion mouseMotion = @event as InputEventMouseMotion;
         if (mouseMotion != null)
         {
-            Neck.RotateY(-mouseMotion.Relative.X * MouseSensivity);
-            Head.Rotation = new Vector3(Mathf.Clamp(Head.Rotation.X - mouseMotion.Relative.Y * MouseSensivity, -Mathf.Pi*0.5f, Mathf.Pi*0.5f), Head.Rotation.Y, Head.Rotation.Z);
+            Neck.RotateY(-mouseMotion.Relative.X * MouseSensitivity);
+            Head.Rotation = new Vector3(Mathf.Clamp(Head.Rotation.X - mouseMotion.Relative.Y * MouseSensitivity, -Mathf.Pi*0.5f, Mathf.Pi*0.5f), Head.Rotation.Y, Head.Rotation.Z);
         }
     }
 }
