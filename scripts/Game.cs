2f01399856efb41bf9904f1d9ffb444c3c1e9409
beepe0 <dshakhun@gmail.com>
2024-02-09 23:58:17 +0100

    Updated voip_system & console_manager & structure the project

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,10 +1,13 @@
-using Godot;
-using System;
+﻿using Godot;
 
 public partial class Game : Node
 {
-    public override void _Ready()
+    public static Game Instance { get; private set; }
+    public static MultiplayerManager MultiplayerManager { get; set; }
+    public static LocalPlayer LocalPlayer { get; set; }
+
+    public override void _EnterTree()
     {
-     
+        Instance = this;
     }
 }
