e31f33f60e2d5d6289243ce2e8d955de137f2952
ihorekghost <158592015+ihorekghost@users.noreply.github.com>
2024-02-12 17:04:03 +0200

    Removed MultiplayerManager.cs, KeyCommandRole class

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -3,9 +3,6 @@
 public partial class Game : Node
 {
     public static Game Instance { get; private set; }
-    public static MultiplayerManager MultiplayerManager { get; set; }
-    public static LocalPlayer LocalPlayer { get; set; }
-
     public override void _EnterTree()
     {
         Instance = this;
